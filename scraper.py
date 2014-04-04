6ff87337d2e961b6303247ffc53efb6eee8c8d03
Luis Capelo <luiscape@gmail.com>
2014-04-03 20:37:28 -0400

    initial

---
+++ scraper.py
@@ -0,0 +1,79 @@
+import codecs
+import sys
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
+from wikihead import headings
+headers = u"""
+Geography
+Climate
+Economy
+Transport
+Education
+Demographics
+Religion
+""".strip().lower().split('\n')
+
+import orm
+"""Value: dsID, region, indID, period, value, source, is_number
+   DataSet: dsID, last_updated, last_scraped, name
+      Indicator: indID, name, units
+         """
+
+dataset = {'dsID': 'wikipedia',
+           'last_updated': None,  # TODO
+           'last_scraped': orm.now(),
+           'name': 'Wikipedia'}
+
+orm.DataSet(**dataset).save()
+
+for h in headers:
+    indicator = {'indID': 'wikipedia:' + h,
+                 'name': 'Wikipedia: ' + h,
+                 'units': 'url'}
+    orm.Indicator(**indicator).save()
+
+value_template = {'dsID': 'wikipedia',
+                  'period': None,
+                  'is_number': False}
+
+
+def exact_match(level, header, country):
+    matches = [x.lower() for x in country[level] if x == header]
+    if len(matches) > 1:
+        print matches, "\n\n\n\n"
+    if matches:
+        return matches[0]
+
+
+def partial_match(level, header, country):
+    matches = []
+    for cand_header in country[level]:
+        if header in cand_header.lower():
+            matches.append(cand_header)
+    if len(matches) > 1:
+        print matches, "\n\n\n\n"
+    if matches:
+        return matches[0]
+
+
+def best_match(header, country):
+    return exact_match(2, header, country) or \
+        exact_match(3, header, country) or \
+        partial_match(2, header, country) or \
+        partial_match(3, header, country)
+
+
+wikibase = "http://en.wikipedia.org/wiki/%s#%s"
+headinglist = headings()
+for country in headinglist:
+    matches = [best_match(header, headinglist[country]) for header in headers]
+    d_matches = dict(zip(headers, matches))
+    for head in d_matches:
+        if d_matches[head]:
+            value = dict(value_template)
+            value['region'] = country
+            value['indID'] = "wikipedia:" + head
+            value['source'] = wikibase % (country, d_matches[head])
+            value['value'] = value['source']
+            print value
+            orm.Value(**value).save()
